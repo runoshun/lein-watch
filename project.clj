03b5246bf6d00922ff8f3e978430be53ac80eba3
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-12-28 12:24:18 -0500

    updated scm in project

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject yogthos/lein-watch "0.0.3"
+(defproject yogthos/lein-watch "0.0.4"
   :description "A Leiningen plugin to watch directories and run tasks automatically."
-  :url "http://github.com/runoshun/lein-watch"
+  :url "http://github.com/yogthos/lein-watch"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :scm {:name "git"
-        :url "https://github.com/runoshun/lein-watch"}
+        :url "https://github.com/yogthos/lein-watch"}
   :eval-in-leiningen true)
